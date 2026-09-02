0ba7e353531f91e0881d81fd7570525ed06fd8f2
Nishant <nishant.gupta@botconsulting.io>
2026-09-02 11:36:28 +0530

    Add new calculator test

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -8,10 +8,10 @@ test("adds two numbers", () => {
   expect(add(2, 3)).toBe(5);
 });
 
-test("subtracts two numbers", () => {
+test("subtract two numbers", () => {
   expect(subtract(5, 3)).toBe(2);
 });
 
-test("multiplies two numbers", () => {
+test("multiplied two numbers", () => {
   expect(multiply(4, 3)).toBe(12);
 });
